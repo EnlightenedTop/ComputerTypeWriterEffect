1ddcac4e73929fe4364ec327cf2fe358c9ce9372
Popa Cristian Alexandru <81997541+EnlightenedTop@users.noreply.github.com>
2022-06-07 15:56:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const text = document.querySelector(".type");
 const btn = document.querySelector(".btn");
 
 let txt = [
-  "a programer/front-end web developer with a keen eye for beautiful ui/functional and satisfying ux",
+  "a programer/front-end web            developer  with a keen eye              for pretty ui/functional                       and  satisfying ux",
 ];
 let i = 0;
-let speed = 80;
+let speed = 60;
 const typeWriter = () => {
-  text.innerHTML = txt[0].substring(0, i) + "<span>&#10072;</span>";
+  text.innerHTML = txt[0].substring(0, i) + "<span>_</span>";
 
   i++ != txt[0].length ? setTimeout(typeWriter, speed) : undefined;
 };
